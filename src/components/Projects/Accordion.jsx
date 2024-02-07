461ae96416b8550cbd73228276882f7a57ca8d28
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-06 19:54:38 -0800

    Add accordion styling

--- src/components/Projects/Accordion.jsx
+++ src/components/Projects/Accordion.jsx
@@ -1,26 +1,57 @@
 import { useState } from "react";
 import portfoliopic from "../../assets/LinkedInPhoto Final.jpg";
 
-function AccordionItem({ curOpen, onOpen, title, img, description }) {
-  const [isOpen, setIsOpen] = useState(false);
+const projectList = [
+  {
+    title: "Frugal Fare",
+    img: portfoliopic,
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, deserunt possimus quae distinctio quod quasi ratione neque
+    praesentium quas iure accusantium, natus voluptate, eaque ipsum
+    provident aperiam eum? Similique, reiciendis?`,
+  },
+  {
+    title: "Frugal Fare",
+    img: portfoliopic,
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, deserunt possimus quae distinctio quod quasi ratione neque
+    praesentium quas iure accusantium, natus voluptate, eaque ipsum
+    provident aperiam eum? Similique, reiciendis?`,
+  },
+  {
+    title: "Frugal Fare",
+    img: portfoliopic,
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, deserunt possimus quae distinctio quod quasi ratione neque
+    praesentium quas iure accusantium, natus voluptate, eaque ipsum
+    provident aperiam eum? Similique, reiciendis?`,
+  },
+  {
+    title: "Frugal Fare",
+    img: portfoliopic,
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, deserunt possimus quae distinctio quod quasi ratione neque
+    praesentium quas iure accusantium, natus voluptate, eaque ipsum
+    provident aperiam eum? Similique, reiciendis?`,
+  },
+];
 
+function AccordionItem({ curOpen, onOpen, num, title, img, children }) {
+  const isOpen = curOpen === num;
+
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
+  }
   return (
     <div
-      className={`shadow-lg  bg-white p-3 rounded-sm mx-8 my-3  text-center flex  flex-col items-center justify-center cursor-pointer ${
+      className={`shadow-sm  bg-white p-3 rounded-sm mx-8 my-3  text-center flex  flex-col items-center justify-center cursor-pointer hover:shadow-md  ${
         isOpen ? "border-blue-900 border-t-4" : ""
       }`}
-      onClick={() => setIsOpen(!isOpen)}
+      onClick={handleToggle}
     >
-      <p className="font-semibold text-blue-900 ">Frugal Fare</p>
+      <p className={` text-blue-900 ${isOpen ? "font-bold" : "font-semibold"}`}>
+        {title}
+      </p>
       {isOpen && (
         <>
-          <img className="size-1/3 p-5" src={portfoliopic} alt="" />
-          <p className="text-slate-600">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo,
-            deserunt possimus quae distinctio quod quasi ratione neque
-            praesentium quas iure accusantium, natus voluptate, eaque ipsum
-            provident aperiam eum? Similique, reiciendis?
-          </p>
+          <img className="size-1/3 p-5" src={img} alt="" />
+          <p className="text-slate-600">{children}</p>
         </>
       )}
     </div>
@@ -32,10 +63,18 @@ function Accordion() {
 
   return (
     <div>
-      <AccordionItem curOpen={curOpen} onOpen={setCurOpen} />
-      <AccordionItem />
-      <AccordionItem />
-      <AccordionItem />
+      {projectList.map(({ title, img, description }, i) => (
+        <AccordionItem
+          key={title}
+          num={i}
+          title={title}
+          img={img}
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+        >
+          {description}
+        </AccordionItem>
+      ))}
     </div>
   );
 }
