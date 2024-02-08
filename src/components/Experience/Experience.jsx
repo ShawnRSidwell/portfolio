abd364fb3162eff6e6114c8496fa9cbaf5d1214b
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-07 17:24:37 -0800

    finish experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,9 +8,10 @@ function Experience() {
           Experience
         </h1>
         <p className="p-1 md:p-6 mb-5 text-center text-slate-500  md:text-lg ">
-          My projects makes use of vast variety of latest technology tools. My
-          best experience is to create Data Science projects and deploy them to
-          web applications using cloud infrastructure.
+          With 2 years of software engineering experience in the technology
+          industry and over a decade of diverse corporate leadership roles in
+          the financial sector, specifically in mortgages, I bring a unique
+          blend of technical proficiency and business acumen to any position.
         </p>
         <div className="flex justify-center">
           <button className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-7 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
