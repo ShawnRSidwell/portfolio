77df47fd0263bad51b8e56bd67043366ebbdddb6
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-07 16:29:52 -0800

    fix padding on Projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -19,7 +19,7 @@ function Projects() {
             best experience is to create Data Science projects and deploy them
             to web applications using cloud infrastructure.
           </p>
-          <div className="grid grid-cols-2 gap-2">
+          <div className="grid grid-cols-2 gap-2 px-5">
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
