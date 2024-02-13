6ac78c166bf931be12dfc4f2a93791baf2613cf0
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-12 18:38:07 -0800

    extra project list to separate file for easy editing

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,37 +1,7 @@
 import { useState } from "react";
 import Button from "../Button";
-import frugalFare from "../../assets/frugal-fare-cropped.png";
-import wildOasis from "../../assets/wild-oasis.png";
-import pizzaMenu from "../../assets/fast-react-pizza.png";
-import eatnSplit from "../../assets/eat-n-split.png";
+import { projectList } from "./projectList";
 import kanbanboard from "../../assets/kanbanboardcropped.png";
-
-const projectList = [
-  {
-    name: "Frugal Fare",
-    description:
-      "A ride-sharing price simulator designed to help users find the most competitive quotes for their trips. By simulating various ride-sharing prices, users can easily compare prices and select the best option tailored to their needs. It is built mainly in Java using MySQL for the databse.",
-    img: frugalFare,
-  },
-  {
-    name: " Wild Oasis",
-    description:
-      "This project is a comprehensive hotel management system developed in Javascript with Supabase acting as the database. Offering robust features and seamless integration, it streamlines hotel operations, including reservations, guest management, billing, and more. ",
-    img: wildOasis,
-  },
-  {
-    name: "Eat-N-Split",
-    description:
-      "This project is a split-check calculator built using React, simplifying the process of dividing bills among multiple individuals. With its intuitive interface and precise calculations, it ensures accurate splitting of expenses, enhancing convenience for users dining together.",
-    img: eatnSplit,
-  },
-  {
-    name: "Shawns Pizza Co.",
-    description:
-      "Developed entirely in React, this project is a fully functioning pizza website offering an immersive online ordering experience. From customizable toppings to seamless checkout, it provides customers with a user-friendly platform to browse, order, and enjoy delicious pizzas from the comfort of their homes.",
-    img: pizzaMenu,
-  },
-];
 
 function Projects() {
   const [projSelected, setProjSelected] = useState(null);
