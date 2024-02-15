fe224131e93b78074e6a5a3d99ebc2c6e2973d8e
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-15 14:51:22 -0800

    Add animation to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { Fade, Slide } from "react-awesome-reveal";
 
 import { projectList } from "./projectList";
 import Button from "../Button";
@@ -21,7 +22,12 @@ function Projects() {
             Projects
           </h2>
           {!projSelected ? (
-            <>
+            <motion.div
+              key="default"
+              initial={{ opacity: 0.5 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
               <img
                 src={kanbanboard}
                 alt="kanban board"
@@ -32,9 +38,15 @@ function Projects() {
                 tools. My best experience is to create Data Science projects and
                 deploy them to web applications using cloud infrastructure.
               </p>
-            </>
+            </motion.div>
           ) : (
-            <>
+            <motion.div
+              key={projSelected.name}
+              layoutId="fadein"
+              initial={{ scale: 0.9 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.25 }}
+            >
               <img
                 src={projSelected.img}
                 alt={projSelected.name}
@@ -43,7 +55,7 @@ function Projects() {
               <p className="p-6 mb-5 text-center text-slate-500 text-lg md:mx-28">
                 {projSelected.description}
               </p>
-            </>
+            </motion.div>
           )}
 
           <div className="grid grid-cols-2 gap-2 md:gap-4 px-5 md:px-24 md:mx-5">
