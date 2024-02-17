d31914e684f4e76662b8cc341290a33345fc5d87
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-16 17:49:27 -0800

    Fix mobile header

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
   const { isSticky } = useHeader();
   return (
     <p
-      className={`hover-underline-animation ${
+      className={`hover-underline-animation text-sm md:text-lg  ${
         isSticky ? "text-blue-900" : "text-blue-900"
       }`}
     >
